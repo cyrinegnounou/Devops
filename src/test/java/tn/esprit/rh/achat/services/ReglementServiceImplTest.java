c8190a7bf22f21bfa41654e8818b8aee61465c1c
MedAmineHm <mohamedamine.hammami@esprit.tn>
2023-10-17 00:33:29 +0100

    Test junit Final

--- src/test/java/tn/esprit/rh/achat/services/ReglementServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ReglementServiceImplTest.java
@@ -14,6 +14,7 @@ import tn.esprit.rh.achat.services.ReglementServiceImpl;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -43,5 +44,45 @@ public class ReglementServiceImplTest {
         Reglement result = reglementService.addReglement(reglement);
 
         assertEquals(reglement, result);
+    }
+    @Test
+    public void testRetrieveAllReglements() {
+        List<Reglement> reglements = Arrays.asList(new Reglement(), new Reglement());
+        when(reglementRepository.findAll()).thenReturn(reglements);
+
+        List<Reglement> result = reglementService.retrieveAllReglements();
+
+        assertEquals(2, result.size());
+    }
+    @Test
+    public void testRetrieveReglement() {
+        Long reglementId = 1L;
+        Reglement reglement = new Reglement();
+        when(reglementRepository.findById(reglementId)).thenReturn(Optional.of(reglement));
+
+        Reglement result = reglementService.retrieveReglement(reglementId);
+
+        assertEquals(reglement, result);
+    }
+    @Test
+    public void testRetrieveReglementByFacture() {
+        Long factureId = 1L;
+        List<Reglement> reglements = Arrays.asList(new Reglement(), new Reglement());
+        when(reglementRepository.retrieveReglementByFacture(factureId)).thenReturn(reglements);
+
+        List<Reglement> result = reglementService.retrieveReglementByFacture(factureId);
+
+        assertEquals(2, result.size());
+    }
+    @Test
+    public void testGetChiffreAffaireEntreDeuxDate() {
+        Date startDate = new Date();
+        Date endDate = new Date();
+        float chiffreAffaire = 1000.0f;
+        when(reglementRepository.getChiffreAffaireEntreDeuxDate(startDate, endDate)).thenReturn(chiffreAffaire);
+
+        float result = reglementService.getChiffreAffaireEntreDeuxDate(startDate, endDate);
+
+        assertEquals(chiffreAffaire, result, 0.01);
     }
 }
