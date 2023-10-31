1d9b591f92b86c58ec34d0b14baf4b3e09eb56a4
Rania ELHOR <rania.elhor@esprit.tn>
2023-11-01 00:30:07 +0100

    added front docker file

--- crud-tuto-front/src/app/shared/Service/Stock.service.ts
+++ crud-tuto-front/src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://192.168.1.12:8080/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.1.12:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
