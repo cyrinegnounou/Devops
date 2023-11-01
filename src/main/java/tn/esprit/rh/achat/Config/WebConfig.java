aa766e801de87fa38a1b645f8905242e6e3e4b19
Rania ELHOR <rania.elhor@esprit.tn>
2023-11-01 01:11:32 +0100

    added front docker file

---
+++ src/main/java/tn/esprit/rh/achat/Config/WebConfig.java
@@ -0,0 +1,21 @@
+package tn.esprit.rh.achat.Config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**").allowedOrigins("http://192.168.1.12:4200").allowedMethods("GET", "POST",
+                "PUT", "DELETE");
+
+    }
+
+}
